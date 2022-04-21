6e9bbf209a2745084e8f8042ba976b395dc007f0
Blocknews <schrempfra@gmail.com>
2022-04-21 20:24:52 +0200

    add router views + links

---
+++ resources/js/routes.js
@@ -0,0 +1,23 @@
+import Home from './components/Home';
+import About from './components/About';
+
+export default {
+    mode: 'history',
+
+    routes: [
+        
+        {
+
+            path: '/',
+            component: Home
+
+        },
+
+        {
+
+            path: '/about',
+            component: About
+            
+        },
+    ]
+};
